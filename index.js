8b31dfbb5d8c3d20a5b9ca2e070462011550c9d5
Omar Nachar <omarnachar2@gmail.com>
2021-04-16 12:23:40 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timegiven){
+  let msg = '';
+  let splt = timegiven.split(':');
+  let hours = parseInt(splt[0]);
+  let min = parseInt(splt[1]);
+  if (hours < 12 && min < 59){
+    msg = 'Good Morning';
+  } else if (hours < 17 && min < 59 ){
+    msg = 'Good Afternoon';
+  } else{
+    msg = 'Good Evening';
+  }
+  return msg;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  let para = document.createElement('p');
+  let t = document.createTextNode(message);
+  para.appendChild(t);
+  document.getElementById('greeting').appendChild(para);
+}
